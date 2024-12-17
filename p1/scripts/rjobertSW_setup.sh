6ea71b8484f5dbfb4111876d25bc86738d8d9d24
Romain Jobert <rjobert@student.42barcelona.com>
2024-12-17 17:40:22 +0100

    first setup 2 VMs by vagrant in macM1 arm64 with vmware

---
+++ p1/scripts/rjobertSW_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Starting server setup..."
+sudo apt-get update
+sudo apt-get install net-tools kubectl
+echo "net-tools & kubectl installed successfully."
---
+++ p1/scripts/rjobertS_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Starting server setup..."
+sudo apt update
+sudo apt install net-tools
+echo "net-tools installed successfully."
