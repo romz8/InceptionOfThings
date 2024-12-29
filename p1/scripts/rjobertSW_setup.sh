ccc1a9c9a4cecd45d08ccfeecce3d9f9a6030a9b
Romain Jobert <rjobert@student.42barcelona.com>
2024-12-29 19:48:53 +0100

    Server and Agent node working - final step : make sure ip / network is forced on setup and vmware setup

--- p1/scripts/rjobertSW_setup.sh
+++ p1/scripts/rjobertSW_setup.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
 
-echo "Starting server setup..."
-sudo apt-get update
-sudo apt-get install net-tools kubectl
-echo "net-tools & kubectl installed successfully."
+# Define variables
+ip="192.168.56.110"
+
+# Update and install dependencies
+sudo apt update
+if [ $? -ne 0 ]; then
+    echo "Failed to update package lists. Exiting."
+    exit 1
+fi
+
+sudo apt install net-tools
+if [ $? -ne 0 ]; then
+    echo "Failed to install net-tools. Exiting."
+    exit 1
+fi
+echo "net-tools & curl installed successfully."
+
+# Retrieve the K3s token from the K3s server
+
+echo "Fetching the K3s token from the server..."
+
+SERVER_TOKEN=$(cat /vagrant/k3s_token)
+if [ -z "$SERVER_TOKEN" ]; then
+    echo "Failed to retrieve the server token. Exiting."
+    exit 1
+fi
+echo "Server token retrieved successfully."
+rm -f /vagrant/k3s_token
+
+# Install K3s worker node
+echo "Installing K3s worker node..."
+sudo curl -sfL https://get.k3s.io | K3S_URL="https://${ip}:6443" K3S_TOKEN="${SERVER_TOKEN}" sh -
+if [ $? -ne 0 ]; then
+    echo "Failed to install K3s worker node. Exiting."
+    exit 1
+fi
+
+echo "K3s worker node installed successfully and running."
--- p1/scripts/rjobertS_setup.sh
+++ p1/scripts/rjobertS_setup.sh
@@ -4,3 +4,18 @@ echo "Starting server setup..."
 sudo apt update
 sudo apt install net-tools
 echo "net-tools installed successfully."
+sudo apt install kubectl
+echo "kubectl installed successfully."
+sudo curl -sfL https://get.k3s.io | sh -
+echo "k3s installed successfully. runnning k3s server..."
+
+# Copy the K3s token to the shared folder
+sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/k3s_token
+if [ $? -ne 0 ]; then
+    echo "Failed to copy the K3s token to the shared folder. Exiting."
+    exit 1
+fi
+
+# Set appropriate permissions for the token
+sudo chmod 644 /vagrant/k3s_token
+echo "K3s token has been copied to the shared folder."
